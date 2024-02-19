5948a17499047caa1681b27dba68dfbc5e869c33
Jeongseup <seup87@gmail.com>
2024-02-19 13:54:29 +0900

    Add cors layer

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,4 +1,4 @@
-use axum::{routing::get, Router};
+use axum::{http::Method, routing::get, Router};
 
 #[cfg(feature = "prometheus")]
 use axum_prometheus::{PrometheusMetricLayerBuilder, AXUM_HTTP_REQUESTS_DURATION_SECONDS};
@@ -6,6 +6,7 @@ use futures_util::{Future, TryFutureExt};
 #[cfg(feature = "prometheus")]
 use metrics_exporter_prometheus::{Matcher, PrometheusBuilder};
 use std::{collections::HashMap, net::SocketAddr};
+use tower_http::cors::{Any, CorsLayer};
 use tracing::{info, instrument};
 
 use crate::config::ServerConfig;
@@ -41,7 +42,12 @@ pub struct ServerState {
 }
 
 fn server_routes(state: ServerState) -> Router<()> {
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET])
+        .allow_origin(Any);
+
     Router::new()
+        .layer(cors)
         .route("/block/height/:block_height", get(get_block_by_height))
         .route("/block/hash/:block_hash", get(get_block_by_hash))
         .route("/block/last", get(get_last_block))
